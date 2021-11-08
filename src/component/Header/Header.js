08dfc31f524b98a9a4946e1359b12c92b3351bde
Pranjal Sharma <ps9395@gmail.com>
2021-11-08 20:39:14 +0530

    initial-2

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,32 +1,53 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Location from '../Location/Location'
+import './Header.css'
 const Header = () => {
     const [inputState, setinputState] = useState('')
+    const [clicked, setclicked] = useState(false)
     const [data, setdata] = useState('')
-    const handleInput=(e)=>{
-          setinputState(e.target.value);
+
+    const handleInput = (e) => {
+        setinputState(e.target.value);
     }
     useEffect(() => {
-        fetch("https://geo.ipify.org/api/v2/country?apiKey=at_4gBaFWnYJDyaykCzghQere9IsAWPv&ipAddress=8.8.8.8")
-        .then((res)=> res.json())
-        .then((res)=>{
-         console.log(res);
-        })
+        fetch("https://geo.ipify.org/api/v2/country?apiKey=at_4gBaFWnYJDyaykCzghQere9IsAWPv")
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+            })
         return () => {
-            
+
         }
     }, [inputState])
 
     return (
-        <div>
+        <div className="header_div">
             <h4>IP Address Tracker</h4>
-            <input type="text"  
-            placeholder="Search for any IP address or domain"
-                value={inputState} onChange={(e)=>{handleInput(e)}}
-            />
-             <Location data={data} />
+            <div className="input_wrapper">
+                <input type="text"
+                    placeholder="Search for any IP address or domain"
+                    value={inputState} onChange={(e) => { handleInput(e) }}
+                />
+                <button>{
+                    clicked?<div className="animated_loader">
+
+                    </div> :
+                    <i className="las la-angle-right"></i>
+                }</button>
+            </div>
+
+            <Location data={data} />
         </div>
     )
 }
 
 export default Header
+
+
+
+
+
+
+
+
+
